f81f9302f50141a347e5415b9d88b5c324f98a73
matheusazambuja <matheusazambuja35@gmail.com>
2021-04-09 17:00:50 -0300

    Add merge between element Motion and Chakra

--- src/types.tsx
+++ src/types.tsx
@@ -1,3 +1,6 @@
+import { chakra, HTMLChakraProps } from "@chakra-ui/system"
+import { HTMLMotionProps, motion } from "framer-motion"
+
 export interface ProductInStock {
   id: number
   name: string
@@ -9,3 +12,11 @@ export interface ProductInStock {
 export interface ProductInCart extends ProductInStock {
   inCart: boolean
 }
+
+
+type Merge<P, T> = Omit<P, keyof T> & T
+type MotionUlProps = Merge<HTMLChakraProps<'ul'>, HTMLMotionProps<'ul'>>
+type MotionLiProps = Merge<HTMLChakraProps<'li'>, HTMLMotionProps<'ul'>>
+
+export const MotionUl: React.FC<MotionUlProps> = motion(chakra.ul)
+export const MotionLi: React.FC<MotionLiProps> = motion(chakra.li)
